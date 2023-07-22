f8e1f03c9048943dc783e90a5e3452462fb09935
muammar200 <amuammar01@gmail.com>
2023-07-23 05:56:04 +0800

    First Commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        \App\Models\Post::factory(1)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // User::create([
+        //     'email' => 'admin@gmail.com',
+        //     'username' => 'admin',
+        //     'firstname' => 'admin',
+        //     'password' => bcrypt('password')
+        // ]);
+    }
+}
